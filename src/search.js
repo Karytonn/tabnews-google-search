2ed895f47bcc0f7a6f7bc28d4dd10369eb8dcdb3
Karytonn <karytonndev@gmail.com>
2022-12-11 13:21:22 -0300

    fix: add new encode method to avoid deletion of characters outside ASCII set

--- src/search.js
+++ src/search.js
@@ -5,18 +5,28 @@ const input = document.querySelector('#search-input');
 const GOOGLE_BASE_URL = "https://www.google.com/search?q=";
 const GOOGLE_HACKING_SITE = "site%3Atabnews.com.br";
 
+const encodeSearchText = (text) =>
+    encodeURIComponent(text)
+        .replace(/\-/g, '%2D')
+        .replace(/\_/g, '%5F')
+        .replace(/\./g, '%2E')
+        .replace(/\!/g, '%21')
+        .replace(/\~/g, '%7E')
+        .replace(/\*/g, '%2A')
+        .replace(/\'/g, '%27')
+        .replace(/\(/g, '%28')
+        .replace(/\)/g, '%29')
+        //by default "#" is encoded to %23
+
 form.addEventListener('submit', (event) =>  {
   event.preventDefault();
   
   if(input.value) {
-     
-    const textFindSanitized = input.value.replace(/\s/g, '%20')
-    const url = `${GOOGLE_BASE_URL}${textFindSanitized}+${GOOGLE_HACKING_SITE}`
-
+    const url = `${GOOGLE_BASE_URL}${encodeSearchText(input.value)}+${GOOGLE_HACKING_SITE}`;
     window.open(url, "_target");
 
   } else {
-    alert("😬 acho que esqueceu de inserir algo no campo de busca.")
+    alert("😬 acho que esqueceu de inserir algo no campo de busca.");
   }
 
 });
